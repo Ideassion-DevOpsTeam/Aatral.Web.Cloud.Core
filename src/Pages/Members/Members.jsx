04a0a08940be93de1eaced05063579ac9a601d14
Imprasna <prasanna@sibbc.org>
2024-05-24 16:31:53 +0530

    features/01-members-removed unused code and console

--- src/Pages/Members/Members.jsx
+++ src/Pages/Members/Members.jsx
@@ -18,21 +18,6 @@ const Members = () => {
         setFounderData();
     }, []);
 
-    // const showModal = (value) => {
-
-    //     if (value.id === isModalOpen) {
-    //         setIsModalOpen(true)
-    //     }
-    //     else {
-    //         console.log("wrong")
-    //     }
-
-    //     console.log(value.id,'val')
-    //     console.log({id: value.id, state: true});
-    //     // setIsModalOpen({id: id, state: true});
-    //     // setIsModalOpen(true);
-    // };
-
     const showModal = (value) => {
         setIsModalOpen({ id: value.id, state: true });
     };
@@ -41,8 +26,6 @@ const Members = () => {
         setIsModalOpen({
             id: '', state: false
         });
-        console.log({id: '', state: false});
-        // setIsModalOpen(false)
     };
 
     return (
@@ -56,55 +39,52 @@ const Members = () => {
                     <div className="members__section__second__section__members">
                         {founderData.map((item, index) => (
                             <Fragment key={index}>
-                            <div onClick={() => showModal(item)} className="members__section__second__section__members--user">
-                                <img src={item?.founder_photo} alt="Founder_Photo" />
-                                <div className="members__section__second__section__members--user-details">
-                                    <p className="members__section__second__section__members--user-details-name">{ item?.owner_name }</p>
-                                    <p className="members__section__second__section__members--user-details-company">{ item?.company_name }</p>
+                                <div onClick={() => showModal(item)} className="members__section__second__section__members--user">
+                                    <img src={item?.founder_photo} alt="Founder_Photo" />
+                                    <div className="members__section__second__section__members--user-details">
+                                        <p className="members__section__second__section__members--user-details-name">{ item?.owner_name }</p>
+                                        <p className="members__section__second__section__members--user-details-company">{ item?.company_name }</p>
+                                    </div>
                                 </div>
-                            </div>
-                                <Modal width="60%"
-                                    open={isModalOpen.id === item.id && isModalOpen.state}
-                                    footer={null} onCancel={handleCancel}>
-        
-                                <div className="member__modal">
-                                    <div className="member__modal__profile">
-                                        <div className="member__modal__profile--picture">
-                                            <img src={item?.founder_photo} alt="Founder_Photo" />
-                                        </div>
-                                        <div className="member__modal__profile--details">
-                                            <div className="member__modal__profile--details-names">
-                                                <p>{ item?.owner_name }</p>
-                                                <p>{ item?.company_name }</p>
+                                <Modal width="60%" open={isModalOpen.id === item.id && isModalOpen.state} footer={null} onCancel={handleCancel}>
+                                    <div className="member__modal">
+                                        <div className="member__modal__profile">
+                                            <div className="member__modal__profile--picture">
+                                                <img src={item?.founder_photo} alt="Founder_Photo" />
                                             </div>
-                                            <div className="member__modal__profile--details-social">
-                                                <span className="member__modal__profile--details-social-link">
-                                                    <a href={item?.website_link} target="_blank" rel="noreferrer"><Icon component={webIcon} /></a>
-                                                </span>
-                                                <span className="member__modal__profile--details-social-link">
-                                                    <a href={item?.instagram} target="_blank" rel="noreferrer"><Icon component={instagram} /></a>
-                                                </span>
-                                                <span className="member__modal__profile--details-social-link">
-                                                    <a href={item?.facebook} target="_blank" rel="noreferrer"><Icon component={faceboonIcon} /></a>
-                                                </span>
-                                                <span className="member__modal__profile--details-social-link">
-                                                    <a href={item?.linkedIn} target="_blank" rel="noreferrer"><Icon component={linkedInIcon} /></a>
-                                                </span>
+                                            <div className="member__modal__profile--details">
+                                                <div className="member__modal__profile--details-names">
+                                                    <p>{ item?.owner_name }</p>
+                                                    <p>{ item?.company_name }</p>
+                                                </div>
+                                                <div className="member__modal__profile--details-social">
+                                                    <span className="member__modal__profile--details-social-link">
+                                                        <a href={item?.website_link} target="_blank" rel="noreferrer"><Icon component={webIcon} /></a>
+                                                    </span>
+                                                    <span className="member__modal__profile--details-social-link">
+                                                        <a href={item?.instagram} target="_blank" rel="noreferrer"><Icon component={instagram} /></a>
+                                                    </span>
+                                                    <span className="member__modal__profile--details-social-link">
+                                                        <a href={item?.facebook} target="_blank" rel="noreferrer"><Icon component={faceboonIcon} /></a>
+                                                    </span>
+                                                    <span className="member__modal__profile--details-social-link">
+                                                        <a href={item?.linkedIn} target="_blank" rel="noreferrer"><Icon component={linkedInIcon} /></a>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="member__modal__description">
+                                            <div className="member__modal__description--texts">
+                                                <p>{ item?.desc }</p>
+                                            </div>
+                                            <div className="member__modal__description--logo">
+                                                <a href={item?.website_link} target="_blank" rel="noreferrer">
+                                                    <img src={item?.company_logo} alt="company-logo" />
+                                                </a>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="member__modal__description">
-                                        <div className="member__modal__description--texts">
-                                            <p>{ item?.desc }</p>
-                                        </div>
-                                        <div className="member__modal__description--logo">
-                                            <a href={item?.website_link} target="_blank" rel="noreferrer">
-                                                <img src={item?.company_logo} alt="company-logo" />
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Modal>
+                                </Modal>
                             </Fragment>
                         ))}
                     </div>   
