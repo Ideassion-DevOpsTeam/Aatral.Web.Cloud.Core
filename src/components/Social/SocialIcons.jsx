4b7fc89142b652501e9caab03d2f594534693dfd
Imprasna <prasanna@sibbc.org>
2024-05-28 16:17:24 +0530

    features/05 - navigation - social links were linked

--- src/components/Social/SocialIcons.jsx
+++ src/components/Social/SocialIcons.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { ReactComponent as Facebook } from "../../assets/Icons/Social/fb.svg";
-import { ReactComponent as Insta } from "../../assets/Icons/Social/insta.svg";
-import { ReactComponent as Linkdin } from "../../assets/Icons/Social/linkden.svg";
-import { ReactComponent as Default } from "../../assets/Icons/Social/own.svg";
+import  { mainFacebookIcon, mainInstagramIcon, mainLinkedInIcon, mainXIcon }  from "../../constants/icons";
+import Icon from '@ant-design/icons';
 
 import "./social.scss";
 
@@ -10,17 +8,17 @@ function SocialIconsComponent() {
   return (
     <section className="social-icon">
       <div className="social-icon__item-box">
-        <Facebook />
-      </div>
-      <div className="social-icon__item-box">
-        <Insta />
-      </div>
-      <div className="social-icon__item-box">
-        <Linkdin />
+        <a href="https://www.facebook.com/profile.php?id=61560240031843" target="_blank" rel="noreferrer"><Icon component={mainFacebookIcon} /></a>
       </div>
       {/* <div className="social-icon__item-box">
-        <Default />
+        <a href="#" target="_blank" rel="noreferrer"><Icon component={mainInstagramIcon} /></a>
       </div> */}
+      <div className="social-icon__item-box">
+        <a href="https://www.linkedin.com/company/aatral-india/about/" target="_blank" rel="noreferrer"><Icon component={mainLinkedInIcon} /></a>
+      </div>
+      <div className="social-icon__item-box">
+        <a href="https://x.com/AatralIndia" target="_blank" rel="noreferrer"><Icon component={mainXIcon} /></a>
+      </div>
     </section>
   );
 }
