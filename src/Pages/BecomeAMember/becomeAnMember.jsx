0cca045620ce47a11b15d485fdd73e36af326c42
Imprasna <prasanna@sibbc.org>
2024-06-03 14:00:47 +0530

    removed the comment

--- src/Pages/BecomeAMember/becomeAnMember.jsx
+++ src/Pages/BecomeAMember/becomeAnMember.jsx
@@ -11,9 +11,6 @@ const BecomeAnMember = () => {
       <SocialIcons />
       <div className="become__a__member__section__block">
         <div className="become__a__member__section__block--top">
-          {/* <p className="become__a__member__section__block--top-coming-soon">
-            Coming soon!
-          </p> */}
           <p className="become__a__member__section__block--top-member-tier">
           Join now to spark connections!
           </p>
